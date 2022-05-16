346fc4296158f511583fddd67fd5273b9ab999f9
asus <836585692@qq.com>
2022-05-16 13:55:45 +0800

    Initial commit

---
+++ src/test/java/com/zeng/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeng.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
