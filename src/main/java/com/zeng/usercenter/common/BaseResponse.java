4176b9b852bdc82b49bf07fb293b15a790b1dc4b
asus <836585692@qq.com>
2022-05-20 22:23:48 +0800

    5.20 五二零加油

--- src/main/java/com/zeng/usercenter/common/BaseResponse.java
+++ src/main/java/com/zeng/usercenter/common/BaseResponse.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 /**
  * @Author 祝英台炸油条
  * @Time : 2022/5/19 16:17
- *
+ * <p>
  * 设置序列化的目的 就是为了完成相应的 可以完成序列化的传输
  **/
 @Data
@@ -21,29 +21,27 @@ public class BaseResponse<T> implements Serializable {
 
     private String description;
 
-    public BaseResponse(int code, T data, String message,String description) {
+    public BaseResponse(int code, T data, String message, String description) {
         this.code = code;
         this.data = data;
         this.message = message;
         this.description = description;
     }
 
-    public BaseResponse(int code, T data,String message) {
-        this(code,data,message,"");
+    public BaseResponse(int code, T data, String message) {
+        this(code, data, message, "");
     }
 
     public BaseResponse(int code, T data) {
-        this(code,data,"","");
+        this(code, data, "", "");
     }
 
-    public BaseResponse(ErrorCode errorCode)
-    {
-        this(errorCode.getCode(),null,errorCode.getMessage(),errorCode.getDescription());
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription());
     }
 
-    public BaseResponse(ErrorCode errorCode,String message,String description)
-    {
-        this(errorCode.getCode(),null,message,description);
+    public BaseResponse(ErrorCode errorCode, String message, String description) {
+        this(errorCode.getCode(), null, message, description);
     }
 
 }
