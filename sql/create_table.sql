9fdb26b2bf8184af946e3dc2559c0be996ce1e2c
asus <836585692@qq.com>
2022-05-21 20:20:52 +0800

    5.21

---
+++ sql/create_table.sql
@@ -0,0 +1,20 @@
+-- auto-generated definition
+create table user
+(
+    user_name     varchar(256)                       null comment '用户昵称',
+    id            bigint auto_increment comment 'id'
+        primary key,
+    user_account  varchar(256)                       null comment '账号',
+    avatar_url    varchar(1024)                      null comment '用户头像',
+    gender        tinyint                            null comment '性别',
+    user_password varchar(512)                       not null comment '密码',
+    phone         varchar(128)                       null comment '电话',
+    email         varchar(512)                       null comment '邮箱',
+    user_status   int      default 0                 not null comment '状态 0-正常',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete     tinyint  default 0                 not null comment '是否删除',
+    user_role     int      default 0                 not null comment '用户角色 0是普通用户  1是管理员
+',
+    planet_code   varchar(512)                       null comment '星球编号'
+);
